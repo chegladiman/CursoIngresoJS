f2d5479289ae4864ca666dd300de5788c3eafe55
chegladiman <facundoariel.dominguez@gmail.com>
2017-06-30 20:23:41 -0300

    Si quiero que me muestre el resultado de una variable escrita al principio, tengo que mandarla abajo del código que tiene las operaciones, para que me las muestre en JS o chrome

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,19 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+    var sueldo;
+    var resultado;
+sueldo=document.getElementById("sueldo").value;
 
-	
+
+sueldo=parseInt(sueldo);
+resultado=parseInt(resultado);
+
+var aumento;
+aumento=0.10;
+var resultadoaumento;
+resultadoaumento=sueldo*aumento;
+var resultadofinal;
+resultadofinal=sueldo+resultadoaumento;
+document.getElementById("resultado").value=resultadofinal;
 }
