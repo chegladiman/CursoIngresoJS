a287e39ee4ed54dd39e71fa85e79fb616f3869a4
chegladiman <facundoariel.dominguez@gmail.com>
2017-06-30 22:08:28 -0300

    mod

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,14 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
+edad=document.getElementById("edad").value;
 
+if (edad>13&&edad<=17)
+{
+
+alert("soy adolescente");
+}
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,15 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
+edad=document.getElementById("edad").value;
+
+if (!edad<13||edad>17)
+{
+
+alert(" NO soy adolescente");
+}
+
 
 
 }//FIN DE LA FUNCIÓN
