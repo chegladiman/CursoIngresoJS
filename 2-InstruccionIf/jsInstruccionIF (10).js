f9355f250338ee106595811aa06bed9e148ee3de
chegladiman <facundoariel.dominguez@gmail.com>
2017-07-02 17:09:19 -0300

    9 y 10 con comentarios

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,22 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var nota;
+
+//var formularioIngreso = document.getElementById("FormIngreso");
+
+nota = Math.floor(Math.random() * 10 + 1);
+
+if(nota>=9)
+{
+	alert('Nota: ' + nota + ' EXCELENTE');
+}
+else if(nota>=4)
+{
+	alert('Nota: ' + nota + ' aprobo');
+}
+else
+	alert('Nota: ' + nota + " vamos la proxima se puede");
+
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -2,5 +2,10 @@ function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+var random;
 
+var formularioIngreso = document.getElementById("FormIngreso");
+
+random = Math.floor(Math.random() * 10 + 1);
+	alert(random);
 }//FIN DE LA FUNCIÓN
