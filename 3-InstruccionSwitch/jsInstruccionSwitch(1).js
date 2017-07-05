2106d85a123d0e1aeaa6e64dc458a1f1c6e9c427
chegladiman <facundoariel.dominguez@gmail.com>
2017-07-04 21:44:43 -0300

    Switch 1 y 2 listos

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -3,9 +3,30 @@ function Mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-{switch(new Date().getMonth())
+
+switch (mesDelAño)
+{
+   
+    case "Enero":
+                alert("que comiences bien el año");
+                break;
+
+    case "Marzo":
+                alert("a clases!!!");
+                break;
+    case "Julio":
+                alert("se vienen las vacaciones de invierno!!!");
+                break;
+
+    case "Diciembre":
+                alert("Felices Fiestas!!!");
+                break;
+
 
     
 
 
-}//FIN DE LA FUNCIÓN
+}
+
+//FIN DE LA FUNCIÓN
+}
--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -5,6 +5,24 @@ var mesDelAño = document.getElementById('mes').value;
 
 alert (mesDelAño);
 
+switch (mesDelAño)
+{
+    case "Septiembre":
+    case "Octubre":
+    case "Noviembre":    
+    case "Diciembre":
+                    alert("ya paso");
+                    break;
+
+    case "Julio":
+    case "Agosto":
+                    alert("abrigate que hace frío!");
+                    break;
+    
+    default:
+            alert("<Falta para el invierno");
+            break;
+}
 
 
 
