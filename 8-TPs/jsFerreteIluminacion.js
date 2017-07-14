ecf2c28330c28f80a2b7ffe57f4e5fd18857f7b7
chegladiman <facundoariel.dominguez@gmail.com>
2017-07-14 20:09:48 -0300

    Ferrete iluminacion ver!!!!

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -9,20 +9,125 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  
  */
 
-    var marca, cantidad, precio, total;
+    
+
+
+   var marca, cantidad, precio, total, totalBrutos, ingresosBrutos;
 precio = 35;
+
+function ImprimirPrecio()
+{
+    
+    if(total >= 120)
+    {
+        ingresosBrutos = total * 0.10;
+        totalBrutos = total + ingresosBrutos;
+        document.getElementById("precioDescuento").value = "$" + totalBrutos;
+        alert("Usted pago $" + ingresosBrutos + " de IIBB");
+    }
+         
+    else
+    {
+        document.getElementById("precioDescuento").value = "$" + total;
+    }
+}
 
 function CalcularPrecio () 
 {
-    marca = document.getElementById("Marca").value;
-    cantidad = document.getElementById("Cantidad").value;
-    
-    if(cantidad >= 6)
-    {
-        total = precio * cantidad * 0,5;
-        document.getElementById("precioDescuento").value = total;
-        
-    }
+ 	 marca = document.getElementById("Marca").value;
+     cantidad = document.getElementById("Cantidad").value;
+     
+     if (cantidad >= 6)
+     {
+         total = precio * cantidad * 0.5;
+         ImprimirPrecio()
+     }
+
+     else
+     {
+         switch(marca)
+         {
+             case "ArgentinaLuz":
+                if(cantidad == 5)
+                {
+                    total = precio * cantidad * 0.6;
+                    ImprimirPrecio()
+                }
                 
+                else if(cantidad == 4)
+                {
+                    total = precio * cantidad * 0.75;
+                    ImprimirPrecio()
+                }
+                
+                else if(cantidad == 3)
+                {
+                    total = precio * cantidad * 0.9;
+                    ImprimirPrecio()
+                }
+
+                else
+                {
+                    total = precio * cantidad;
+                    ImprimirPrecio()
+                }
+                break;
+
+            case "FelipeLamparas":
+                if(cantidad == 5)
+                {
+                    total = precio * cantidad * 0.7;
+                    ImprimirPrecio()
+                }
+                
+                else if(cantidad == 4)
+                {
+                    total = precio * cantidad * 0.75;
+                    ImprimirPrecio()
+                }
+                
+                else if(cantidad == 3)
+                {
+                    total = precio * cantidad * 0.85;
+                    ImprimirPrecio()
+                }
+                
+                else
+                {
+                    total = precio * cantidad;
+                    ImprimirPrecio()
+                }
+                break;
             
+            case "JeLuz":
+            case "AzIluminacion":
+            case "Osram":
+                if(cantidad == 5)
+                {
+                    total = precio * cantidad * 0.70;
+                    ImprimirPrecio()
+                }
+                
+                else if(cantidad == 4)
+                {
+                    total = precio * cantidad * 0.80;
+                    ImprimirPrecio()
+                }
+                
+                else if (cantidad == 3)
+                {
+                    total = precio * cantidad * 0.95;
+                    ImprimirPrecio()
+                }
+                
+                else
+                {
+                    total = precio * cantidad;
+                    ImprimirPrecio()
+                }
+                break;
+        }
+     }
 }
+            
+
