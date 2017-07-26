7ffd222eb3c01dfc4d2f272277b017619db9aa11
chegladiman <facundoariel.dominguez@gmail.com>
2017-07-25 21:31:07 -0300

    6 y 7 con mods

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,7 +1,12 @@
 function Mostrar()
 {
+    var repeticiones=parseInt(prompt("Ingresar número"))
 
-
+    for(contador=0;contador<=repeticiones;contador++)
+    if(contador%2==0)
+    {
+        console.log("Numero Par: " +contador);
+    }
 
 
 }//FIN DE LA FUNCIÓN
--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,7 +1,17 @@
 function Mostrar()
 {
 
+ var repeticiones=parseInt(prompt("Ingresar número"))
 
+    for(contador=1;contador<=repeticiones;contador++)
+    if(repeticiones%contador==0)
+    {
+            contador++
+        if(contador>2)
+        console.log("Numero divisor: " +contador +"\n");
+        break;
+        
+    }
 
 
 }//FIN DE LA FUNCIÓN
